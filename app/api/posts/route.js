24fbbb267f27fa59c7565aa53341be6640581e3d
MBaroky <m.baroky@gmail.com>
2025-01-02 05:54:08 +0200

    Blog done with single post pages and using markup files for better control

--- app/api/posts/route.js
+++ app/api/posts/route.js
@@ -1,47 +1,55 @@
-const endpoint = [
-  {
-    post_id: 1,
-    image: "post-1.jpeg",
-    title: "Investing in Your Future with Real Estate",
-    desc: "Discover the potential of real estate investments. Learn how Solitaire Crest...",
-    slug: "",
-  },
-  {
-    post_id: 2,
-    image: "post-2.jpeg",
-    title: "Navigating the Real Estate Market",
-    desc: "Stay informed with expert insights on market trends, property values, and...",
-    url: "",
-  },
-  {
-    post_id: 3,
-    image: "post-3.jpeg",
-    title: "Maximizing Value in Property Investments",
-    desc: "Explore strategies to increase property value and maximize your investments...",
-    url: "",
-  },
-  {
-    post_id: 4,
-    image: "post-4.jpeg",
-    title: "Homebuyer’s Guide: From Dream to Reality",
-    desc: "Take the first steps in your homebuying journey with our in-depth guides, tips...",
-    url: "",
-  },
-  {
-    post_id: 5,
-    image: "post-5.jpeg",
-    title: "Building Your Real Estate Portfolio",
-    desc: "Whether you're a first-time investor or a seasoned pro, our tips on building a...",
-    url: "",
-  },
-  {
-    post_id: 6,
-    image: "post-6.jpeg",
-    title: "Spotlight on Neighborhoods: Finding Your Ideal Location",
-    desc: "Renowned for high-quality Explore in-depth profiles of prime neighborhoods...",
-    url: "",
-  },
-];
+import { paths } from "@/lib/posts";
+
+const postsPaths = paths;
+const endpoint = postsPaths.map(
+  async path => (await import(`@/posts/${path}`))?.metadata
+);
+console.log(endpoint);
+
+// const endpoint = [
+//   {
+//     post_id: 1,
+//     image: "post-1.jpeg",
+//     title: "Investing in Your Future with Real Estate",
+//     desc: "Discover the potential of real estate investments. Learn how Solitaire Crest...",
+//     slug: "",
+//   },
+//   {
+//     post_id: 2,
+//     image: "post-2.jpeg",
+//     title: "Navigating the Real Estate Market",
+//     desc: "Stay informed with expert insights on market trends, property values, and...",
+//     url: "",
+//   },
+//   {
+//     post_id: 3,
+//     image: "post-3.jpeg",
+//     title: "Maximizing Value in Property Investments",
+//     desc: "Explore strategies to increase property value and maximize your investments...",
+//     url: "",
+//   },
+//   {
+//     post_id: 4,
+//     image: "post-4.jpeg",
+//     title: "Homebuyer’s Guide: From Dream to Reality",
+//     desc: "Take the first steps in your homebuying journey with our in-depth guides, tips...",
+//     url: "",
+//   },
+//   {
+//     post_id: 5,
+//     image: "post-5.jpeg",
+//     title: "Building Your Real Estate Portfolio",
+//     desc: "Whether you're a first-time investor or a seasoned pro, our tips on building a...",
+//     url: "",
+//   },
+//   {
+//     post_id: 6,
+//     image: "post-6.jpeg",
+//     title: "Spotlight on Neighborhoods: Finding Your Ideal Location",
+//     desc: "Renowned for high-quality Explore in-depth profiles of prime neighborhoods...",
+//     url: "",
+//   },
+// ];
 
 export async function GET(req, res) {
   return new Response(JSON.stringify(endpoint), {
